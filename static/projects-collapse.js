6fe7492bb703299deecea462256fb3b6ff8f0659
srky <srky420@gmail.com>
2023-11-02 21:14:03 +0500

    Add new certification

--- static/projects-collapse.js
+++ static/projects-collapse.js
@@ -4,18 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#see-more-btn').addEventListener('click', (e) => {
         document.querySelectorAll('.project-hidden').forEach(project => {
             project.classList.toggle('project-show');
-            
-            // Toggle show more/less link text
-            if (TOGGLE === 0) {
-                e.currentTarget.innerHTML = `Show Less <span class="arrow-backward">&lt</span>`;
-                e.currentTarget.removeAttribute('href');
-                TOGGLE = 1;
-            }
-            else {
-                e.currentTarget.innerHTML = 'Show More <span class="arrow-forward">&gt</span>';
-                e.currentTarget.setAttribute('href', '#projects');
-                TOGGLE = 0;
-            }
         });
+        
+        // Toggle show more/less link text
+        if (TOGGLE === 0) {
+            e.currentTarget.innerHTML = `Show Less <span class="arrow-backward">&lt</span>`;
+            e.currentTarget.removeAttribute('href');
+            TOGGLE = 1;
+        }
+        else {
+            e.currentTarget.innerHTML = 'Show More <span class="arrow-forward">&gt</span>';
+            e.currentTarget.setAttribute('href', '#projects');
+            TOGGLE = 0;
+        }
     });
 });
